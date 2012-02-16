b67739ee7fd257570e566ae621c9b43e4ecee802
Milton Madanda <milton@praekeltfoundation.org>
2012-02-16 10:37:15 +0200

    renamed YALComments to Comments - 1 more

--- jmbocomments/__init__.py
+++ jmbocomments/__init__.py
@@ -1,7 +1,7 @@
 def get_model():
-    from jmbocomments.models import YALComment
-    return YALComment
+    from jmbocomments.models import Comment
+    return Comment
 
 def get_form():
-    from jmbocomments.forms import YALCommentForm
-    return YALCommentForm
+    from jmbocomments.forms import CommentForm
+    return CommentForm
